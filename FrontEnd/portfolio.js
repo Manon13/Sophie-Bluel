07cbb89ab8e5ba3ca09aa19edcc363c6fc3eda72
Manon <manonn.goliot@gmail.com>
2024-03-22 20:10:15 +0100

    create a filter function

--- FrontEnd/portfolio.js
+++ FrontEnd/portfolio.js
@@ -1,21 +1,26 @@
 //Fonction pour récupérer les projets depuis l'API
-async function recoverProjects(){
+async function recoverProjects() {
     const response = await fetch('http://localhost:5678/api/works');
     const works = await response.json();
     console.log(works);
     return works;
 }
 
+
 async function generateProject() {
     const works = await recoverProjects();
     for (let i = 0; i < works.length; i++) {
         const project = works[i];
+
         //Récupération de l'élément DOM qui contiendra la gallery des projets
         const divGallery = document.querySelector("#gallery");
+
         //Création d'une balise dédié à un projet
         const figureProjet = document.createElement("figure");
+
         //Ajout de la classe "projet" à la balise figure
         figureProjet.classList.add("project");
+        figureProjet.dataset.categoryName = project.category.name;
 
         //Création d'une balise img pour l'image du projet
         const imgProjet = document.createElement("img");
@@ -24,7 +29,7 @@ async function generateProject() {
 
         //Création d'une balise figcaption pour le titre du projet
         const figcaptionProjet = document.createElement("figcaption");
-        figcaptionProjet.innerText = projet.title;
+        figcaptionProjet.innerText = project.title;
 
         //On rattache la balise figure à la div de gallery
         divGallery.appendChild(figureProjet);
@@ -32,6 +37,88 @@ async function generateProject() {
         figureProjet.appendChild(imgProjet);
         figureProjet.appendChild(figcaptionProjet);
     }
+    return works;
+}
+generateProject();
+
+
+async function generateCategory() {
+    const response = await fetch('http://localhost:5678/api/categories');
+    const category = await response.json();
+    console.log(category);
+    return category;
+}
+generateCategory();
+
+
+async function filterCategory() {
+
+    const category = await generateCategory();
+
+    const divFilter = document.createElement("div");
+    const divGallery = document.querySelector(".gallery");
+    const sectionPortfolio = document.querySelector("#portfolio");
+    sectionPortfolio.insertBefore(divFilter, divGallery);
+
+    const allButton = document.createElement("button");
+    allButton.innerText = "Tous";
+    divFilter.appendChild(allButton);
+
+    for (let i = 0; i < category.length; i++) {
+        const filter = category[i];
+        const buttonFilter = document.createElement("button");
+        buttonFilter.innerText = filter.name;
+        divFilter.appendChild(buttonFilter);
+    }
+
+    //Test de la fonctionnalité de filtre 
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const projects = document.querySelectorAll(".project");
+            projects.forEach(project => {
+                if (button.innerText === "Tous") {
+                    project.style.display = "block";
+                } else if (project.dataset.categoryName !== button.innerText) {
+                    project.style.display = "none";
+                } else {
+                    project.style.display = "block";
+                }
+            });
+        });
+    });
 }
 
-generateProject();
+filterCategory();
+
+
+
+// async function filterCategory() {
+//     const filterCategory = await generateProject();
+
+//     //Création d'une balise div pour les filtres
+//     const divFilter = document.createElement("div");
+
+//     const sectionPortfolio = document.querySelector("#portfolio");
+//     const divGallery = document.querySelector(".gallery");
+//     sectionPortfolio.insertBefore(divFilter, divGallery);
+
+//     //Ajout de la classe "projet" à la balise figure
+//     divFilter.classList.add("filters");
+
+//     for (let i = 0; i < filterCategory.length; i++) {
+//         const filter = filterCategory[i];
+//         //Récupération de l'élément DOM qui contiendra la section "mes projets"
+//         const sectionPortfolio = document.querySelector("#portfolio");
+//         //Création d'une balise button pour chaque filtre
+//         const buttonFilter = document.createElement("button");
+//         buttonFilter.innerText = filter.categoryId;
+
+//         //On rattache la balise div à la section de portfolio
+//         sectionPortfolio.appendChild(divFilter);
+//         //On rattache les boutons à la div
+//         divFilter.appendChild(buttonFilter);
+//     }
+// }
+
+// filterCategory();
