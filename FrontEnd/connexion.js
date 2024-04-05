f43e53fdba044900d5fed5a12db1b371ea585298
Manon <manonn.goliot@gmail.com>
2024-04-05 11:44:40 +0200

    redirect user to login function

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,6 +1,6 @@
-function functionConnexion() {
+function connexion() {
     const formConnexion = document.querySelector('.formConnexion');
-    formConnexion.addEventListener("submit", function(event){
+    formConnexion.addEventListener("submit", function (event) {
         event.preventDefault();
 
         //Récupération des données du formulaire
@@ -19,7 +19,48 @@ function functionConnexion() {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(connexion)
         });*/
+
+        // Appel de la fonction fetch avec toutes les informations nécessaires
+        // Création de la charge utile au format JSON
+        console.log(JSON.stringify(connexion));
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                "accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(connexion)
+        })
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Le serveur a rencontré un problème : " + response.status);
+            }
+            return response.json();
+        })
+
+        .then(data => {
+            console.log(data);
+            // Redirection vers la page d'accueil
+            localStorage.setItem("token", data.token);
+
+            if (data.error) {
+                document.location.href = "./login.html";
+            } else {
+                localStorage.setItem("token", data.token);
+                document.location.href = "./index.html"
+            }
+        })
+
+        .catch(error => {
+            console.error(error);
+        });
+
     });
+
 }
 
-functionConnexion()
+connexion()
+
+
+
