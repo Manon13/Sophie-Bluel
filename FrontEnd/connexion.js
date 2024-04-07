263f6325a3b4d175dfdd84af3f5dd149f83ebe2a
Manon <manonn.goliot@gmail.com>
2024-04-07 18:50:13 +0200

    creation of checkEmptyFields() and showLoginError()

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,7 +1,8 @@
 function connexion() {
-    const formConnexion = document.querySelector('.formConnexion');
-    formConnexion.addEventListener("submit", function (event) {
+    const form = document.querySelector('.formConnexion');
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
+        checkEmptyFields();
 
         //Récupération des données du formulaire
         const connexion = {
@@ -9,24 +10,9 @@ function connexion() {
             password: event.target.querySelector("[name=password]").value
         };
 
-        const spanError = document.createElement("span");
-        spanError.classList.add("error");
-        spanError.innerText = "L'email ou le mot de passe est incorrecte. Veuillez réessayer.";
-        formConnexion.appendChild(spanError);
-
         console.log(JSON.stringify(connexion));
-
         // Appel de la fonction fetch avec toutes les informations nécessaires
         // Création de la charge utile au format JSON
-        /**fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(connexion)
-        });*/
-
-        // Appel de la fonction fetch avec toutes les informations nécessaires
-        // Création de la charge utile au format JSON
-        console.log(JSON.stringify(connexion));
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: {
@@ -38,16 +24,15 @@ function connexion() {
 
             .then(response => {
                 if (!response.ok) {
+                    showLoginError();
                     throw new Error("Le serveur a rencontré un problème : " + response.status);
                 }
                 return response.json();
             })
 
             .then(data => {
-                console.log(data);
-                // Redirection vers la page d'accueil
+                // Stockage du token dans le localStorage
                 localStorage.setItem("token", data.token);
-
                 if (data.error) {
                     document.location.href = "./login.html";
                 } else {
@@ -61,13 +46,11 @@ function connexion() {
             });
     });
 }
-connexion()
+connexion();
 
 
+function checkEmptyFields() {
 const form = document.querySelector('.formConnexion');
-
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
 
     const baliseEmail = document.querySelector('#email');
     const valeurEmail = baliseEmail.value;
@@ -75,37 +58,19 @@ form.addEventListener('submit', function (event) {
     const valeurPassword = balisePassword.value;
     
     if (valeurEmail === '' || valeurPassword === '') {
-        alert('Veuillez remplir tous les champs');
+        const spanChampsVide = document.createElement("span");
+        spanChampsVide.classList.add("error");
+        spanChampsVide.innerText = "Veuillez remplir tous les champs";
+        form.appendChild(spanChampsVide);
         return;
     }
+};
 
-});
 
-//TEST
-// function deconnexion() {
-//     const btnDeconnexion = document.querySelector('.btnDeconnexion');
-//     btnDeconnexion.addEventListener("click", function (event) {
-//         event.preventDefault();
-//         localStorage.removeItem("token");
-//         document.location.href = "./login.html";
-//     });
-// }
-
-// deconnexion();
-
-// function updateMenu() {
-//     const token = localStorage.getItem("token");
-//     const loginTab = document.querySelector('.loginTab');
-//     const logoutTab = document.querySelector('.logoutTab');
-
-//     if (token) {
-//         loginTab.style.display = "none";
-//         logoutTab.style.display = "block";
-//     } else {
-//         loginTab.style.display = "block";
-//         logoutTab.style.display = "none";
-//     }
-// }
-
-// updateMenu();
-
+function showLoginError (){
+    const form = document.querySelector('.formConnexion');
+    const spanError = document.createElement("span");
+    spanError.classList.add("error");
+    spanError.innerText = "L'email ou le mot de passe est incorrecte. Veuillez réessayer.";
+    form.appendChild(spanError);
+};
